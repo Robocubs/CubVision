6a12ca315dcc2bae546d849289c764315cbad2d4
tjkr0wn <joumaa_family@icloud.com>
2024-03-26 00:58:36 -0400

    Messy, but functional, setup script

---
+++ launch_cubvision_base.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# !!! ONLY EXECUTE THIS SCRIPT DIRECTLY WHEN TESTING, DO NOT TOUCH
+# !!! IT SHOULD LAUNCH AUTOMATICALLY ON STARTUP
+
+# Put commands here to launch one or more CubVision instances. This shell script is run at startup by /etc/rc.local
+# /etc/rc.local should run this script. If it isn't, use `setup_cubvision.py`
+# sudo systemctl status rc-local.service to check the status
